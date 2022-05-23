5732014665f2dc938c919ce79fa7469f9ac581ce
Sahani <sahani070797@gmail.com>
2022-05-23 11:54:23 +0200

    navigation updated

--- app/src/main/java/com/example/ezcooking/navigation/RecipeNavigation.kt
+++ app/src/main/java/com/example/ezcooking/navigation/RecipeNavigation.kt
@@ -1,30 +1,48 @@
 package com.example.ezcooking.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.ezcooking.screens.detail.DetailScreen
 import com.example.ezcooking.screens.favourite.FavouriteScreen
 
 import com.example.ezcooking.screens.home.HomeScreen
 import com.example.ezcooking.screens.list.ListScreen
 import com.example.ezcooking.screens.search.SearchScreen
+import com.example.ezcooking.viewmodels.RecipeViewModel
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
+    val recipeViewModel: RecipeViewModel = viewModel()
 
     NavHost(
         navController = navController,
         startDestination = RecipeScreens.HomeScreen.name
     ) {
         composable(RecipeScreens.HomeScreen.name) {
-            HomeScreen(navController = navController)
+            HomeScreen(
+                viewModel = recipeViewModel,
+                navController = navController
+            )
         }
 
         composable(
             RecipeScreens.FavouriteScreen.name) {
-            FavouriteScreen(navController = navController)
+            FavouriteScreen(
+                viewModel = recipeViewModel,
+                navController = navController
+            )
+        }
+
+        composable(
+            RecipeScreens.DetailScreen.name) {
+            DetailScreen(
+                viewModel = recipeViewModel,
+                navController = navController
+            )
         }
 
         composable(
